c5f0c95186a944ccda605cd8e0f19e9090daf55e
MichaaelVacca <michaelvacca07@gmail.com>
2023-05-13 15:34:45 -0400

    fixed aggregate

--- orders-service/src/main/java/com/fooddelivery/ordersservice/presentationlayer/OrderResponseModel.java
+++ orders-service/src/main/java/com/fooddelivery/ordersservice/presentationlayer/OrderResponseModel.java
@@ -2,15 +2,24 @@ package com.fooddelivery.ordersservice.presentationlayer;
 
 import com.fooddelivery.ordersservice.datalayer.Items;
 import com.fooddelivery.ordersservice.datalayer.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class OrderResponseModel {
+    private String orderId;
     private String clientId;
     private String restaurantId;
     private String menuId;
-    private String driverId;
+    private String deliveryDriverId;
     private String driverFirstName;
     private String driverLastName;
     private String clientUsername;
